e54b50a2e74982323fd5ff08c1d66688db2f20c1
Tristan Deschamps <tris790@gmail.com>
2023-05-31 22:25:05 -0400

    Init

---
+++ src/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const c = @cImport(@cInclude("raylib.h"));
+
+pub fn main() !void {
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    c.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    c.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!c.WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        c.BeginDrawing();
+        {
+            c.ClearBackground(c.RAYWHITE);
+            c.DrawText("Congrats! You created your first window!", 190, 200, 20, c.LIGHTGRAY);
+        }
+        c.EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    c.CloseWindow(); // Close window and OpenGL context
+}
