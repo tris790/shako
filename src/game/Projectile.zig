5a5275f0341ca1f7b14826d2e8d6bd7e22f51d4e
Tristan Deschamps <tris790@gmail.com>
2023-06-03 16:51:26 -0400

    Add special ability

--- src/game/Projectile.zig
+++ src/game/Projectile.zig
@@ -7,12 +7,11 @@ const TransformComponent = @import("../components/TransformComponent.zig");
 const TextureComponent = @import("../components/TextureComponent.zig");
 const CollisionComponent = @import("../components/CollisionComponent.zig");
 
-pub fn spawn(ecs: *Ecs, position: c.Vector2, direction: c.Vector2, velocity: c.Vector2) void {
+pub fn spawn(ecs: *Ecs, position: c.Vector2, direction: c.Vector2, velocity: c.Vector2, texture: TextureComponent) void {
     const width = 20;
     const height = 20;
     const projectileMovementComponent = MovementComponent{ .direction = direction, .velocity = velocity };
     const projectileTransformComponent = TransformComponent{ .position = position, .scale = c.Vector2{ .x = width / 2, .y = height / 2 } };
-    const projectile_texture_component = TextureComponent{ .color = c.BLACK };
     const projectile_collision_component = CollisionComponent{
         .ignore = 0,
         .hitbox = c.Rectangle{
@@ -23,5 +22,5 @@ pub fn spawn(ecs: *Ecs, position: c.Vector2, direction: c.Vector2, velocity: c.V
         },
     };
 
-    _ = ecs.createEntity(.{ projectileMovementComponent, projectileTransformComponent, projectile_texture_component, projectile_collision_component });
+    _ = ecs.createEntity(.{ projectileMovementComponent, projectileTransformComponent, texture, projectile_collision_component });
 }
--- src/systems/InputSystem.zig
+++ src/systems/InputSystem.zig
@@ -4,6 +4,7 @@ const c = @cImport(@cInclude("raylib.h"));
 const Ecs = @import("../ecs/Ecs.zig");
 
 const TransformComponent = @import("../components/TransformComponent.zig");
+const TextureComponent = @import("../components/TextureComponent.zig");
 const MovementComponent = @import("../components/MovementComponent.zig");
 const Projectile = @import("../game/Projectile.zig");
 
@@ -15,7 +16,11 @@ pub fn hasToggledDebug() bool {
     return c.IsKeyPressed(c.KEY_H);
 }
 
-pub fn movement() c.Vector2 {
+pub fn hasPressedSpecialAbility() bool {
+    return c.IsKeyPressed(c.KEY_V);
+}
+
+pub fn movementDirection() c.Vector2 {
     const right = @intCast(i32, @boolToInt(c.IsKeyDown(c.KEY_RIGHT)));
     const left = @intCast(i32, @boolToInt(c.IsKeyDown(c.KEY_LEFT)));
     const up = @intCast(i32, @boolToInt(c.IsKeyDown(c.KEY_UP)));
@@ -32,17 +37,45 @@ pub fn movement() c.Vector2 {
 pub fn run(ecs: *Ecs) void {
     var transform_component: *TransformComponent = ecs.getComponent(TransformComponent, 0);
     var movement_component: *MovementComponent = ecs.getComponent(MovementComponent, 0);
-    movement_component.direction = movement();
+    movement_component.direction = movementDirection();
 
     if (movement_component.direction.x != 0 or movement_component.direction.y != 0) {
         movement_component.last_direction = movement_component.direction;
     }
 
     if (isShooting()) {
-        Projectile.spawn(ecs, transform_component.position, movement_component.last_direction, c.Vector2{ .x = 5, .y = 5 });
+        const texture = TextureComponent{ .color = c.BLACK };
+        Projectile.spawn(ecs, transform_component.position, movement_component.last_direction, c.Vector2{ .x = 5, .y = 5 }, texture);
     }
 
     if (hasToggledDebug()) {
         ecs.debug.render_hitboxes = !ecs.debug.render_hitboxes;
+    }
+
+    if (hasPressedSpecialAbility()) {
+        var remaining_projectiles: u32 = 10;
+        var angle_step = 2 * std.math.pi / @intToFloat(f32, remaining_projectiles);
+        const colors = [_]c.Color{ c.PINK, c.GREEN };
+        while (remaining_projectiles > 0) {
+            const angle = angle_step * @intToFloat(f32, remaining_projectiles);
+            const opposite = std.math.sin(angle);
+            const adjacent = std.math.cos(angle);
+            const direction = c.Vector2{
+                .x = opposite,
+                .y = adjacent,
+            };
+
+            const color_index = remaining_projectiles % colors.len;
+            std.log.info("shooting: {} angle: {} direction: {}", .{ color_index, @floatToInt(u32, angle), direction });
+            const texture = TextureComponent{ .color = colors[color_index] };
+            Projectile.spawn(
+                ecs,
+                transform_component.position,
+                direction,
+                c.Vector2{ .x = 5, .y = 5 },
+                texture,
+            );
+            remaining_projectiles -= 1;
+        }
     }
 }
