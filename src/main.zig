7187ab16865f665b008375e2614d3ffb128a3c77
Tristan Deschamps <tris790@gmail.com>
2024-06-02 15:14:31 -0400

    add env variables comment

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const c = @cImport(@cInclude("raylib.h"));
 const Game = @import("game/Game.zig");
 
+// __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia
+
 pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
